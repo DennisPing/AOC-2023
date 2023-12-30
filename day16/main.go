82b778f5658be9f0123a26e180100bb288c0424a
Dennis Ping <idennisping@gmail.com>
2023-12-30 01:10:45 -0500

    Fixed day 16 redundant logic

--- day16/main.go
+++ day16/main.go
@@ -24,7 +24,7 @@ func main() {
 
 func part1(grid []string) {
 	start := Vector{0, -1, 0, 1} // Start at [0, -1] going right
-	visited := walk(grid, []Vector{start})
+	visited := walk(grid, start)
 
 	// Only count [r, c]. Ignore directions and the starting cell.
 	cells := make(map[[2]int]bool)
@@ -51,7 +51,7 @@ func part2(grid []string) {
 
 	maxTiles := 0
 	for _, startingVec := range startingVecs {
-		visited := walk(grid, []Vector{startingVec})
+		visited := walk(grid, startingVec)
 
 		// Only count [r, c]. Ignore directions and the starting cell.
 		cells := make(map[[2]int]bool)
@@ -63,55 +63,53 @@ func part2(grid []string) {
 	fmt.Println(maxTiles)
 }
 
-func walk(grid []string, startVecs []Vector) map[Vector]bool {
+func walk(grid []string, startVec Vector) map[Vector]bool {
 	numRows := len(grid)
 	numCols := len(grid[0])
 
 	visited := make(map[Vector]bool)
 	stack := deque.New[Vector]()
 
-	for _, startVec := range startVecs {
-		stack.PushBack(startVec)
+	stack.PushBack(startVec)
 
-		// DFS
-		// 1. Move the vector to the next point.
-		// 2. Then check bounds.
-		// 3. Then check cell symbol.
-		// 4. Then add the new vector to the stack with the correct displacement (dr/dc).
-		for stack.Len() > 0 {
-			vec := stack.PopBack()
-			if !visited[vec] {
-				visited[vec] = true
+	// DFS
+	// 1. Move the vector to the next point.
+	// 2. Then check bounds.
+	// 3. Then check cell symbol.
+	// 4. Then add the new vector to the stack with the correct displacement (dr/dc).
+	for stack.Len() > 0 {
+		vec := stack.PopBack()
+		if !visited[vec] {
+			visited[vec] = true
 
-				// Look at the vector's next point
-				r, c, dr, dc := vec.r, vec.c, vec.dr, vec.dc
-				r += dr // Next r
-				c += dc // Next c
+			// Look at the vector's next point
+			r, c, dr, dc := vec.r, vec.c, vec.dr, vec.dc
+			r += dr // Next r
+			c += dc // Next c
 
-				if !isValidPosition(r, c, numRows, numCols) {
-					continue
-				}
+			if !isValidPosition(r, c, numRows, numCols) {
+				continue
+			}
 
-				if grid[r][c] == '.' || (grid[r][c] == '-' && dc != 0) || (grid[r][c] == '|' && dr != 0) { // Going same direction
-					stack.PushBack(Vector{r, c, dr, dc})
+			if grid[r][c] == '.' || (grid[r][c] == '-' && dc != 0) || (grid[r][c] == '|' && dr != 0) { // Going same direction
+				stack.PushBack(Vector{r, c, dr, dc})
 
-				} else if grid[r][c] == '|' && dc != 0 { // Going horizontal and hit vertical bar
-					stack.PushBack(Vector{r, c, -1, 0})
-					stack.PushBack(Vector{r, c, 1, 0})
+			} else if grid[r][c] == '|' && dc != 0 { // Going horizontal and hit vertical bar
+				stack.PushBack(Vector{r, c, -1, 0})
+				stack.PushBack(Vector{r, c, 1, 0})
 
-				} else if grid[r][c] == '-' && dr != 0 { // Going vertical and hit horizontal bar
-					stack.PushBack(Vector{r, c, 0, -1})
-					stack.PushBack(Vector{r, c, 0, 1})
+			} else if grid[r][c] == '-' && dr != 0 { // Going vertical and hit horizontal bar
+				stack.PushBack(Vector{r, c, 0, -1})
+				stack.PushBack(Vector{r, c, 0, 1})
 
-				} else if grid[r][c] == '/' { // Col dir becomes the new inverted row dir. Row dir becomes the new interved col dir
-					stack.PushBack(Vector{r, c, -dc, -dr})
+			} else if grid[r][c] == '/' { // Col dir becomes the new inverted row dir. Row dir becomes the new interved col dir
+				stack.PushBack(Vector{r, c, -dc, -dr})
 
-				} else if grid[r][c] == '\\' { // Col dir becomes the new row dir. Row dir becomes the new col dir.
-					stack.PushBack(Vector{r, c, dc, dr})
+			} else if grid[r][c] == '\\' { // Col dir becomes the new row dir. Row dir becomes the new col dir.
+				stack.PushBack(Vector{r, c, dc, dr})
 
-				} else {
-					log.Printf("Hit an unhandled edge case: %v\n", grid[r][c])
-				}
+			} else {
+				log.Printf("Hit an unhandled edge case: %v\n", grid[r][c])
 			}
 		}
 	}
